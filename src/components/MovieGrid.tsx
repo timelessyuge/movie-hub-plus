9409cadb39f8e387d8f9fbd3df6b058b7c96b0fd
timelessyuge <timelessyuge@gmail.com>
2025-01-16 12:23:42 +0800

    Implement movie skeletons while loading

--- src/components/MovieGrid.tsx
+++ src/components/MovieGrid.tsx
@@ -1,23 +1,25 @@
-import { SimpleGrid, Spinner, Text } from "@chakra-ui/react";
-import useMovies from "../hooks/useMovies";
+import { SimpleGrid, Text } from "@chakra-ui/react";
+import useMovies, { MovieQuery } from "../hooks/useMovies";
 import MovieCard from "./MovieCard";
 import MovieCardContainer from "./MovieCardContainer";
-import { MovieQuery } from "../hooks/useMovies";
+import MovieCardSkeleton from "./MovieCardSkeleton";
 
 interface Props {
   movieQuery: MovieQuery;
 }
 
 const MovieGrid = ({ movieQuery }: Props) => {
+  const skeletons = Array.from({ length: 20 }, (_, i) => i + 1);
   const { data: movies, error, isLoading } = useMovies(movieQuery);
   if (error) return error;
-  if (isLoading) return <Spinner />;
   return movies.length > 0 ? (
     <SimpleGrid
       columns={{ sm: 1, md: 2, lg: 3, xl: 4 }}
       spacing={6}
       marginY={2}
     >
+      {isLoading &&
+        skeletons.map((skeleton) => <MovieCardSkeleton key={skeleton} />)}
       {movies.map((movie) => (
         <MovieCardContainer key={movie.id}>
           <MovieCard movie={movie} />
